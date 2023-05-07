f9d20bd26ea8058aa44a02ecc9c3704084cb21e4
Jose24Ma <101275019+Jose24Ma@users.noreply.github.com>
2023-05-07 16:39:38 -0500

    Update funciones.js

--- FigurasG-main/figurasg/src/main/webapp/Assets/js/funciones.js
+++ FigurasG-main/figurasg/src/main/webapp/Assets/js/funciones.js
@@ -9,18 +9,18 @@ function obtenerAreaDecagono() {
     const areaDecagono = perimetroDecagono * apotemaDecagono / 2;
     document.getElementById("areaResultado").value = areaDecagono;
 }
-function PerimetroRombo() {
+function obtenerPerimetroRombo() {
     const ladoRombo = parseInt(document.getElementById("lado").value);
     const perimetroRombo = ladoRombo * 4;
     document.getElementById("perimetroResultado").value = perimetroRombo;
 }
-function AreaRombo() {
+function obtenerAreaRombo() {
     const diagonalMayor = parseInt(document.getElementById("diagonalmayor").value);
     const diagonalMenor = parseInt(document.getElementById("diagonalmenor").value);
     const areaRombo = diagonalMayor * diagonalMenor / 2;
     document.getElementById("areaResultadoRombo").value = areaRombo;
 }
-function EncontrarPerimetroPentagono() {
+function obtenerPerimetroPentagono() {
     const ladoPentagono = parseInt(document.getElementById("lado").value);
     const perimetroPentagono = ladoPentagono * 5;
     document.getElementById("perimetroResultado").value = perimetroPentagono;
