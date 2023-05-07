ec9d3ba766dc8f7d017604874dfe1c221c53b4d7
Jose24Ma <101275019+Jose24Ma@users.noreply.github.com>
2023-05-07 16:50:50 -0500

    Update funciones.js

--- FigurasG-main/figurasg/src/main/webapp/Assets/js/funciones.js
+++ FigurasG-main/figurasg/src/main/webapp/Assets/js/funciones.js
@@ -22,20 +22,20 @@ function calcularPerimetroFigura() {
 
 function calcularAreaFigura() {
   const figura = document.getElementById("figura").value;
-  let area = 0;
+  let areaR,areaD = 0;
   
   switch(figura) {
     case "decagono":
       const perimetroDecagono = parseInt(document.getElementById("perimetroResultado").value);
       const apotemaDecagono = parseInt(document.getElementById("apotema").value);
-      area = perimetroDecagono * apotemaDecagono / 2;
-      document.getElementById("areaResultado").value = area;
+      areaD = perimetroDecagono * apotemaDecagono / 2;
+      document.getElementById("areaResultado").value = areaD;
       break;
     case "rombo":
       const diagonalMayor = parseInt(document.getElementById("diagonalmayor").value);
       const diagonalMenor = parseInt(document.getElementById("diagonalmenor").value);
-      area = diagonalMayor * diagonalMenor / 2;
-      document.getElementById("areaResultadoRombo").value = area;
+      areaR = diagonalMayor * diagonalMenor / 2;
+      document.getElementById("areaResultadoRombo").value = areaR;
       break;
     default:
       console.error("Figura no soportada");
