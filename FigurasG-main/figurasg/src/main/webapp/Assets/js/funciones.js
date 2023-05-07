e4245c7dd78742c297f795ba250bf2fc7ad3076d
Jose24Ma <101275019+Jose24Ma@users.noreply.github.com>
2023-05-07 16:08:02 -0500

    Add files via upload

--- FigurasG-main/figurasg/src/main/webapp/Assets/js/funciones.js
+++ FigurasG-main/figurasg/src/main/webapp/Assets/js/funciones.js
@@ -11,18 +11,18 @@ function obtenerPerimetro() {
 }
 function obtenerPerimetroRombo() {
     const ladoRombo = parseInt(document.getElementById("lado").value);
-    const perimetro = ladoRombo * 4;
-    document.getElementById("perimetroResultado").value = perimetro;
+    const perimetroRombo = ladoRombo * 4;
+    document.getElementById("perimetroResultado").value = perimetroRombo;
 }
 function obtenerAreaRombo() {
     const diagonalMayor = parseInt(document.getElementById("diagonalmayor").value);
     const diagonalMenor = parseInt(document.getElementById("diagonalmenor").value);
-    const area = diagonalMayor * diagonalMenor / 2;
-    document.getElementById("areaResultadoRombo").value = area;
+    const areaRombo = diagonalMayor * diagonalMenor / 2;
+    document.getElementById("areaResultadoRombo").value = areaRombo;
 }
 function obtenerPerimetroPentagono() {
     const ladoPentagono = parseInt(document.getElementById("lado").value);
-    const perimetro = ladoPentagono * 5;
-    document.getElementById("perimetroResultado").value = perimetro;
+    const perimetroPentagono = ladoPentagono * 5;
+    document.getElementById("perimetroResultado").value = perimetroPentagono;
 }
 
